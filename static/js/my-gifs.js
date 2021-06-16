55e7fde2c48edb162dc98f2b3610d6361a57f163
cuncunfacu <64096405+cuncunfacu@users.noreply.github.com>
2021-06-16 15:40:24 -0300

    checkpoint mygifs

--- static/js/my-gifs.js
+++ static/js/my-gifs.js
@@ -1,9 +1,29 @@
 import {apiKey, baseUrl} from './settings.js';
 import {trendingGifsComponent} from './trending-gifs-component.js';
 import {navBarComponent} from './nav-bar-component.js';
+import {miniCardsComponent} from './mini-cards-component.js';
+import {getFavs} from './api-calls.js';
 
 let trendingGifsComponentDiv = document.getElementById('trending-gifs-component');
 let navBarComponentDiv = document.getElementById('nav-bar-component');
+let favoritesMiniCards = document.getElementById('mygifs-mini-cards');
 
-trendingGifsComponent(trendingGifsComponentDiv);
+let favoriteGifsIds = [];
+favoriteGifsIds.push(['9lEGNc2hPkmevAciHq', 'l2olcETxXQjImhNcm2', '3oxQNhG6QjONT91Ga4', 'TSIsZjiAPbFBFNNaJj'])
+localStorage.clear()
+localStorage.setItem('favoriteGifsIds', JSON.stringify(favoriteGifsIds))
+
+
 navBarComponent(navBarComponentDiv);
+trendingGifsComponent(trendingGifsComponentDiv);
+
+let favsIds = getFavs()
+console.log(favsIds)
+if (favsIds.length != 0) {
+    miniCardsComponent(favoritesMiniCards, favsIds);
+}else {
+    // todo render no favss view
+    let p = document.createElement('p');
+    p.innerText = 'No FAVS'
+    favoritesMiniCards.appendChild(p)
+}
