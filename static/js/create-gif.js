d3c4b37bfc606306da079a155e9b32d008713c91
cuncunfacu <64096405+cuncunfacu@users.noreply.github.com>
2021-07-13 18:47:37 -0300

    cammera access stream

--- static/js/create-gif.js
+++ static/js/create-gif.js
@@ -1,6 +1,8 @@
 import {navBarComponent} from './nav-bar-component.js';
 import {isDarkMode, renderDarkMode} from './api-calls.js';
 
+let video = document.createElement('video');
+video.id = 'gif-vid';
 let navBarComponentDiv = document.getElementById('nav-bar-component');
 let beginButton = document.getElementById('btn-comenzar');
 let contentBox = document.getElementById('content-box');
@@ -9,12 +11,35 @@ let oval1 = document.getElementById('oval1');
 let oval2 = document.getElementById('oval2');
 let oval3 = document.getElementById('oval3');
 
-const handleBeggin = () => {
+const record = (stream) => {
+    let videoEl = document.getElementById('gif-vid');
+    //connect the media stream to the first video element
+    if ("srcObject" in videoEl) {
+        videoEl.srcObject = stream;
+    } else {
+        //old version
+        videoEl.src = window.URL.createObjectURL(mediaStreamObj);
+    }
+    videoEl.onloadedmetadata = () => {
+        video.play();
+    };
+}
+const handleBeggin = async () => {
     document.getElementById('content-h1').innerText = '¿Nos das acceso a tu cámara?';
     document.getElementById('content-p').innerText = 'El acceso a tu camara será válido sólo por el tiempo en el que estés creando el GIFO.'
     oval1.src = 'static/images/paso-a-paso-hover1.svg';
     beginButton.remove()
+    let stream = null;
+    try {
+        stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: { height: { max: 200 } } });
+        /* use the stream */
+        content.appendChild(video);
+        record(stream)
+    } catch (err) {
+        /* handle the error */
+    }
 }
+
 beginButton.addEventListener('click', async () => {
     handleBeggin();
 })
